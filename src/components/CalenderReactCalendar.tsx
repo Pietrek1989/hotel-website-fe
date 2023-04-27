b8370e563a4f86534910d1b673e50a89e7f54c1f
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-04-27 14:19:23 +0200

    trying the react calendar

--- src/components/CalenderReactCalendar.tsx
+++ src/components/CalenderReactCalendar.tsx
@@ -7,36 +7,18 @@ const CalendarComponent = () => {
   const [selectedRange, setSelectedRange] = React.useState<any>(null);
   const [selectStep, setSelectStep] = React.useState(0);
 
-  const adjustDate = (date: Date) => {
-    const cetOffsetInHours = 1; // CET is UTC+1
-    const dstOffsetInHours = isDateInDST(date) ? 1 : 0; // Check if the date is in DST and apply an additional hour offset
-    const totalOffsetInMs = (cetOffsetInHours + dstOffsetInHours) * 3600000;
-    return new Date(date.getTime() + totalOffsetInMs);
-  };
-
-  const isDateInDST = (date: Date) => {
-    const january = new Date(date.getFullYear(), 0, 1);
-    const july = new Date(date.getFullYear(), 6, 1);
-    const stdTimezoneOffset = Math.max(
-      january.getTimezoneOffset(),
-      july.getTimezoneOffset()
-    );
-    return date.getTimezoneOffset() < stdTimezoneOffset;
-  };
-
   const handleDateClick = (date: Date) => {
-    const adjustedDate = adjustDate(date);
-
     if (selectStep === 0) {
-      setSelectedRange({ start: adjustedDate, end: null });
+      setSelectedRange({ start: date, end: null });
       setSelectStep(1);
     } else {
-      if (adjustedDate < selectedRange.start) {
-        setSelectedRange({ start: adjustedDate, end: null });
-      } else {
-        setSelectedRange({ ...selectedRange, end: adjustedDate });
-        setSelectStep(0);
-      }
+      const end = date;
+      const range = {
+        start: selectedRange.start,
+        end: end > selectedRange.start ? end : selectedRange.start,
+      };
+      setSelectedRange(range);
+      setSelectStep(0);
     }
   };
 
@@ -46,27 +28,15 @@ const CalendarComponent = () => {
       selectedRange &&
       ((selectedRange.start &&
         !selectedRange.end &&
-        isSameDate(date, selectedRange.start)) ||
+        date.getTime() === selectedRange.start.getTime()) ||
         (selectedRange.start &&
           selectedRange.end &&
-          (isSameDate(date, selectedRange.start) ||
-            isSameDate(date, selectedRange.end) ||
-            isDateInRange(date, selectedRange.start, selectedRange.end))));
+          date >= selectedRange.start &&
+          date <= selectedRange.end));
 
     return isSelected ? "selected" : "";
   };
 
-  const isSameDate = (date1: Date, date2: Date) => {
-    return (
-      date1.getFullYear() === date2.getFullYear() &&
-      date1.getMonth() === date2.getMonth() &&
-      date1.getDate() === date2.getDate()
-    );
-  };
-
-  const isDateInRange = (date: Date, startDate: Date, endDate: Date) => {
-    return date >= startDate && date <= endDate;
-  };
   return (
     <>
       <div className="calendar-container">
